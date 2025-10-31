2db884477f0e225b534d79920b6790ae5d0587bb
rikiulirniam <rikiulir@gmail.com>
2025-10-31 13:28:31 +0700

    navbar

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,8 +4,9 @@ export function Dashboard(){
     return (
         <>
             <Navbar />
-            <section className="hero-section linear-upside-down py-12">
+            <section className="hero-section pt-12 border-1">
                 <img src="/assets/img/herosection.jpg" alt="-" />
+                <div className="linear-upside-down w-screen h-full absolute top-0 ">s</div>
             </section>
             <section>
                 
